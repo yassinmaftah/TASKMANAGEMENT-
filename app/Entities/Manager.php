d1470efc0e928fdb8369c0b08084935b72bdac37
yassinmftah <yassinmaftah06@gmail.com>
2026-01-09 15:31:50 +0100

    create class Manager extands TeamMember

--- app/Entities/Manager.php
+++ app/Entities/Manager.php
@@ -4,6 +4,6 @@ class Manager extends TeamMember
 {
     public function canCreateProject(){return true;}
     public function canAssignTasks(){return true;}
-    public function getRolePermissions(){return ['create_project', 'assign_tasks'];}
+    public function getRolePermissions(){return ['create_project', 'assign_tasks'];} 
 }
 ?>
