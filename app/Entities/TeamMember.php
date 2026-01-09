6c3cdb239d2b03c9afa2df24a9ed98a5db52f8ea
yassinmftah <yassinmaftah06@gmail.com>
2026-01-09 12:12:19 +0100

    Create class abstract named TeamMember with All Properties and Getters and two methods verifyPassword() setPassword()

---
+++ app/Entities/TeamMember.php
@@ -0,0 +1,28 @@
+<?php
+
+abstract class TeamMember
+{
+    private int $id;
+    private string $username;
+    private string $email;
+    private int $teamId;
+    private DateTime $createdAt;
+    private string $password;
+
+    public function Getid() { return $this->id ;}
+    public function GetUserName() { return $this->username ;}
+    public function GetEmail() { return $this->email ;}
+    public function GetTeamID() { return $this->teamId ;}
+    public function GetCreateDate() { return $this->createdAt ;}
+
+    abstract public function canCreateProject();
+    abstract public function canAssignTasks();
+    abstract public function getRolePermissions();
+
+    public function verifyPassword($password) 
+    {
+        if (password_verify($password , $this->password)) {return true;} return false; 
+    }
+    public function setPassword($password) 
+    { $this->password = password_hash($password, PASSWORD_DEFAULT);;}
+}
